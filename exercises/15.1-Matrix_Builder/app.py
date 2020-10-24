6e85783889b2287c501f7fa7196b2f2947252196
jdglez91 <jdglez1991@gmail.com>
2020-10-24 01:19:08 +0000

    incomplete

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,18 @@
 #Import random
 
 #Create the function below:
+def matrixBuilder(integer):
+    list1 = []   
+    for i in range(integer):
+        list1.append([])
+        for n in range(integer):
+            list1[i].append(1)
+        
+    print(list1)
+
+matrixBuilder(5)    
+
+
+        
+
+       
